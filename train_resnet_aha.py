29b2ba768d40ff9696aee18075ff1d666667c795
kule155155 <161822648+kule155155@users.noreply.github.com>
2026-07-31 20:44:41 +0800

    Update train_resnet_aha.py

--- train_resnet_aha.py
+++ train_resnet_aha.py
@@ -333,7 +333,7 @@ if __name__ == '__main__':
 
     acc = accuracy_score(all_labels, all_preds)
     precision = precision_score(all_labels, all_preds, average='macro', zero_division=0)
-    recall = recall_score(all_labels, average='macro', zero_division=0)
+    recall = recall_score(all_labels, all_preds, average='macro', zero_division=0)
     f1 = f1_score(all_labels, all_preds, average='macro', zero_division=0)
     cm = confusion_matrix(all_labels, all_preds)
 
